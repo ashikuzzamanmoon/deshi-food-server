872ef22d19cb8d665d26ffd916711d32d874f73e
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-03 01:19:03 +0600

    edit json file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express(); 
 const port = process.env.PORT || 5000;
 const data=require('./recipe.json');
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Deshi Food')
@@ -9,7 +12,12 @@ app.get('/', (req, res) => {
 
 app.get('/chef',(req,res)=>{
     res.send(data)
+});
 
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedData = data.find(pd => pd.id === id);
+    res.send(selectedData)
 })
 
 app.listen(port, () => {
